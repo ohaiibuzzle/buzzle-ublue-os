2a3f44cdaddea860375ff27b3ec9e3ceb612f116
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-01-10 23:28:10 +0700

    remove kernel package

--- build_files/base/02-kernel_akmods.sh
+++ build_files/base/02-kernel_akmods.sh
@@ -5,7 +5,7 @@
 set -eoux pipefail
 
 # Remove Existing Kernel
-for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
     rpm --erase $pkg --nodeps
 done
 
