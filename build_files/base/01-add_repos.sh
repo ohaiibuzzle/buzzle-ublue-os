aedd6deb299141eac1451055d7ee24f723eb5713
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-03-26 18:54:02 +0700

    add: ublue packages repos

--- build_files/base/01-add_repos.sh
+++ build_files/base/01-add_repos.sh
@@ -3,6 +3,7 @@
 set -eoux pipefail
 
 dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enable ublue-os/packages
 dnf5 -y copr enable sentry/switcheroo-control_discrete
 dnf5 -y copr enable che/nerd-fonts
 dnf5 -y copr enable gmaglione/podman-bootc
--- build_files/base/09-finalize.sh
+++ build_files/base/09-finalize.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable ublue-os/packages
 dnf5 -y copr disable sentry/switcheroo-control_discrete
 dnf5 -y copr disable che/nerd-fonts
 dnf5 -y copr disable gmaglione/podman-bootc
