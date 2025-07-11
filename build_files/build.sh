334ef82fab545ab2e0410fe6f87d2044a75225fa
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-07-12 00:21:31 +0700

    fix: force execution with bash

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,19 +5,19 @@ set -eoux pipefail
 # Run scripts
 
 # shellcheck disable=SC1091
-/ctx/build_files/base/01-add_repos.sh
+/usr/bin/bash /ctx/build_files/base/01-add_repos.sh
 
 # shellcheck disable=SC1091
-/ctx/build_files/base/02-install_packages.sh
+/usr/bin/bash /ctx/build_files/base/02-install_packages.sh
 
 # shellcheck disable=SC1091
-/ctx/build_files/base/03-customizations.sh
+/usr/bin/bash /ctx/build_files/base/03-customizations.sh
 
 # shellcheck disable=SC1091
-/ctx/build_files/base/04-brew.sh
+/usr/bin/bash /ctx/build_files/base/04-brew.sh
 
 # shellcheck disable=SC1091
-/ctx/build_files/base/05-finalize.sh
+/usr/bin/bash /ctx/build_files/base/05-finalize.sh
 
 mkdir -p /var/lib &&
     mkdir -p /var/tmp &&
