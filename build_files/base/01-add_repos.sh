7e5b8028657f88a637f1ad452fe4820ccc74da51
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-10-31 20:36:02 +0700

    fix: switcheroo

--- build_files/base/01-add_repos.sh
+++ build_files/base/01-add_repos.sh
@@ -4,7 +4,6 @@ set -eoux pipefail
 
 dnf5 -y copr enable ublue-os/staging
 dnf5 -y copr enable ublue-os/packages
-dnf5 -y copr enable sentry/switcheroo-control_discrete
 dnf5 -y copr enable che/nerd-fonts
 dnf5 -y copr enable gmaglione/podman-bootc
 dnf5 -y copr enable solopasha/hyprland
